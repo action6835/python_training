38922a156eadc1326a41c4e1f4bc9c7e242a8b53
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-03 21:19:46 +0300

    test_add_new - создание вспомогательных методов

--- test_add_new.py
+++ test_add_new.py
@@ -12,15 +12,15 @@ class AddNew(unittest.TestCase):
     
     def test_add_new(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_contact(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_contact(self, wd):
         # init add new
         wd.find_element_by_link_text("add new").click()
         # fill form
@@ -77,8 +77,17 @@ class AddNew(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("notes")
         # submit
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
