8e4eae7955ff9bdc6a073655ddba1e0698d1103a
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-11 16:03:26 +0300

    Предусловия для тестов удаления и модификации групп

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_mod_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(name="New group"))
 
 
 def test_mod_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(header="New header"))
 
 
 def test_mod_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(footer="New footer"))
