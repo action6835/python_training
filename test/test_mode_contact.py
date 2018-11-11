8b165b815f4eeea73610738f1fd08bdbb50d5d76
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-11 16:40:55 +0300

    Оптимизация ввода данных в форму групп

--- test/test_mode_contact.py
+++ test/test_mode_contact.py
@@ -1,10 +1,17 @@
 from model.contact import Contact
 
 
-def test_mod_contact(app):
-    app.contact.modify_first(
-        Contact(firstname="FIRST NAME", middlename="INITIAL", lastname="LAST NAME", nickname="NICKNAME", title="TITLE",
-                company="COMPANY", address="ADDRESS", home="111-111",
-                mobile="222-222", work="333-333", fax="444-444", email="111-111@m.ru", email2="222-222@m.ru", email3="333-333@m.ru",
-                homepage="HOMEPAGE", address2="ADDRESS2", phone2="555-555",
-                notes="NOTES"))
+def test_mod_contact_firstname(app):
+    app.contact.modify_first(Contact(firstname="New firstname"))
+
+
+def test_mod_contact_lastname(app):
+    app.contact.modify_first(Contact(lastname="New lastname"))
+
+
+def test_mod_contact_address(app):
+    app.contact.modify_first(Contact(address="New address"))
+
+
+def test_mod_contact_email(app):
+    app.contact.modify_first(Contact(email="New email"))
