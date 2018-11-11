8cb1a3bc2a0cca14912ea413fa05d7d9af2a297a
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-11 17:21:32 +0300

    оптимизация переходов по страницам

--- fixture/contact.py
+++ fixture/contact.py
@@ -64,7 +64,8 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add"))) > 0:
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
