bcaf9a18eb2117e497f30849f26c73d6c17a3d5f
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-11 16:49:04 +0300

    предусловия для тестов удаления и модификации контактов

--- test/test_mode_contact.py
+++ test/test_mode_contact.py
@@ -2,16 +2,24 @@ from model.contact import Contact
 
 
 def test_mod_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first(Contact(firstname="New firstname"))
 
 
 def test_mod_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first(Contact(lastname="New lastname"))
 
 
 def test_mod_contact_address(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first(Contact(address="New address"))
 
 
 def test_mod_contact_email(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first(Contact(email="New email"))
