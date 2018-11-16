f7ebdcee7ea31b9f6f4515e4e3adfebccc0a6e77
action6835 <44606418+action6835@users.noreply.github.com>
2018-11-16 18:15:31 +0300

    Проверки, сравнение списков групп

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -5,16 +5,30 @@ from model.group import Group
 def test_mod_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.modify_first(Group(name="New group"))
+
+    old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.modify_first(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_mod_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_mod_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(footer="New footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
