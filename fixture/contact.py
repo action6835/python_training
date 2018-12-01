51ac1c76328b7d7879eead6c7798c83ac873bfb1
action6835 <44606418+action6835@users.noreply.github.com>
2018-12-01 13:48:22 +0300

    Добавлен test_phones_on_home_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -94,8 +94,23 @@ class ContactHelper:
                 firstname = cells[2].text
                 lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, home=all_phones[0],
+                                                  mobile=all_phones[1], work=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, home=homephone, mobile=mobilephone,
+                       work=workphone, phone2=secondaryphone, id=id)
 
     def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
