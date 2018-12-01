a2aaa61d4d597b0701556ed854efe580f8973e2a
action6835 <44606418+action6835@users.noreply.github.com>
2018-12-01 14:09:37 +0300

    Добавлен test_phones_on_contact_view_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -111,6 +112,16 @@ class ContactHelper:
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, home=homephone, mobile=mobilephone,
                        work=workphone, phone2=secondaryphone, id=id)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(home=homephone, mobile=mobilephone, work=workphone, phone2=secondaryphone)
 
     def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
