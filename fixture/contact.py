5a35cf33537e7f31fa23001400064caa22ee5791
action6835 <44606418+action6835@users.noreply.github.com>
2018-12-01 15:19:58 +0300

    проверки для всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -94,10 +94,13 @@ class ContactHelper:
                 cells = element.find_elements_by_tag_name('td')
                 firstname = cells[2].text
                 lastname = cells[1].text
+                address = cells[3].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
-                                                  all_phones_frome_home_page=all_phones))
+                all_emails = cells[4].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, address=address, id=id,
+                                                  all_phones_from_home_page=all_phones,
+                                                  all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
     def get_contact_info_from_edit_page(self, index):
@@ -105,13 +108,17 @@ class ContactHelper:
         self.open_contact_to_edit_by_index(index)
         firstname = wd.find_element_by_name("firstname").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
         homephone = wd.find_element_by_name("home").get_attribute("value")
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, home=homephone, mobile=mobilephone,
-                       work=workphone, phone2=secondaryphone, id=id)
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, address=address, home=homephone, mobile=mobilephone,
+                       work=workphone, email=email, email2=email2, email3=email3, phone2=secondaryphone, id=id)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
--- model/contact.py
+++ model/contact.py
@@ -4,7 +4,8 @@ from sys import maxsize
 class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None,
-                 homepage=None, address2=None, phone2=None, notes=None, id=None, all_phones_frome_home_page=None):
+                 homepage=None, address2=None, phone2=None, notes=None, id=None, all_phones_from_home_page=None,
+                 all_emails_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -24,7 +25,8 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
-        self.all_phones_frome_home_page = all_phones_frome_home_page
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
